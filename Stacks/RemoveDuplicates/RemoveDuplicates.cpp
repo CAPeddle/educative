b0e81be513980f25852db6a45e2f006581ca0bdc
Christopher Peddle <christopher.peddle@asml.com>
2025-01-21 13:54:34 +0100

    move methods out of main obj

--- Stacks/RemoveDuplicates/RemoveDuplicates.cpp
+++ Stacks/RemoveDuplicates/RemoveDuplicates.cpp
@@ -5,25 +5,13 @@
 #include <algorithm>
 #include "RemoveDuplicates.h"
 
-
-int main()
-{
-    std::string test("kaYak");
-    std::string result = (RemoveDuplicates::IsPalindrome(test) ? "is" : "is not");
-    std::cout << test << result << "palindrome\n";
-
-    std::unordered_map<int, std::vector<int> > test_vect =
-    {
-        {-1, { 1, -1, 0 }},
-        {10, { 3,7,1,2,8,4,5 }},
-        {21, { 3,7,1,2,8,4,5 }},
-        {-8, {-1,2,1,-4,5,-3}}
-    };
-
-    for (auto& vector_test : test_vect) {
-        std::string result = (RemoveDuplicates::FindSumOfThree(vector_test.second, vector_test.first) ? " is " : " is not ");
-        std::cout << " 3 Sum " << result << " possible \n";
-    }
+std::string RemoveDuplicates(std::string s) {
+    
+    // Replace this placeholder return statement with your code
+    return "";
 }
+
+
+
 
 
---
+++ Stacks/RemoveDuplicates/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include "RemoveDuplicates.h"
+
+
+
+int main()
+{
+    std::string test("kaYak");
+    std::string result = (RemoveDuplicates::IsPalindrome(test) ? "is" : "is not");
+    std::cout << test << result << "palindrome\n";
+
+}
+
+

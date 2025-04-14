184e051666861413398ef76b394fd2c1367f1ba9
Christopher Peddle <christopher.peddle@asml.com>
2025-04-14 15:52:29 +0200

    Add Binary tree skeleton

---
+++ Trees/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+#include "BinaryTree.cpp"
+#include <string>
+#include <iostream>
+
+// DiameterOfBinaryTree returns the diameter of tree
+int DiameterOfBinaryTree(TreeNode<int> *root)
+{
+
+    // Replace this placeholder return statement with your code
+    return -1;
+}
+
+int main()
+{
+    std::vector<std::vector<TreeNode<int>*>> listOfTrees = { {new TreeNode<int>(2), new TreeNode<int>(1), new TreeNode<int>(4), new TreeNode<int>(3), new TreeNode<int>(5), new TreeNode<int>(6), new TreeNode<int>(7)},
+                    { new TreeNode<int>(1), new TreeNode<int>(2), new TreeNode<int>(3), new TreeNode<int>(4), new TreeNode<int>(5), new TreeNode<int>(6), new TreeNode<int>(7), new TreeNode<int>(8), new TreeNode<int>(9)},
+                    { new TreeNode<int>(45), new TreeNode<int>(32), new TreeNode<int>(23), new TreeNode<int>(21), new TreeNode<int>(18), new TreeNode<int>(1)},
+                    { new TreeNode<int>(5), new TreeNode<int>(3), new TreeNode<int>(4), new TreeNode<int>(1), new TreeNode<int>(2), new TreeNode<int>(6), new TreeNode<int>(7), new TreeNode<int>(8), new TreeNode<int>(9)},
+                    { new TreeNode<int>(9), new TreeNode<int>(7), nullptr, nullptr, new TreeNode<int>(1), new TreeNode<int>(8), new TreeNode<int>(10), nullptr, new TreeNode<int>(12)}
+    };
+
+    int y = 1;
+    for (const BinaryTree<int> tree : listOfTrees) {
+        std::cout << y << ".\tBinary tree:\n";
+        // DisplayTree(tree.root);
+        std::cout << "\tDiameter of the tree: " << DiameterOfBinaryTree(tree.root) << "\n";
+        std::cout << std::string(100, '-') << std::endl;
+        y++;
+    }
+}
